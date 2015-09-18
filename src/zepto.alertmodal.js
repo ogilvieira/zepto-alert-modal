b594c3ce8815872ed87c67fd73da90d477ef7ee5
Gil Vieira <ogilvieira@gmail.com>
2015-09-18 15:05:15 +0000

    initial commit

---
+++ src/zepto.alertmodal.js
@@ -0,0 +1,22 @@
+'use strict';
+/**
+*
+* zepto.alertmodal.js
+* @author: Gil Vieira
+*
+**/
+
+(function($){
+	var defaults = {
+		type: 'danger',
+		valign: 'top',
+		align: 'center',
+		interval: 8000,
+		animation: 'slide',
+		title: false
+	}
+
+	function $.zeptoalert( text, options ) {
+		// magic here
+	};	
+}( Zepto || jQuery ));
